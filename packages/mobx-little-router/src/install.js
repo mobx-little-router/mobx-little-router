994b1a825303cf652ff46c903a6eb578c837bf8a
Jack Hsu <jack.hsu@gmail.com>
2017-08-15 15:25:19 -0400

    Returns Router instance from install.

--- packages/mobx-little-router/src/install.js
+++ packages/mobx-little-router/src/install.js
@@ -7,24 +7,11 @@ import type { HistoryCreatorFn } from './Router'
 import type { Config } from './routing/types'
 import type { History } from 'history'
 
-type Module = {
-  history: History,
-  store: RouterStore,
-  start: () => void,
-  stop: () => void
-}
-
 type Options = {
   createHistory: HistoryCreatorFn | [HistoryCreatorFn, Object],
   routes: Config[]
 }
 
-export default function install(opts: Options): Module {
-  const manager = new Router(opts.createHistory, opts.routes.map(createRouteNode))
-  return {
-    history: manager.history,
-    store: manager.store,
-    start: () => manager.start(),
-    stop: () => manager.stop()
-  }
+export default function install(opts: Options): Router {
+  return new Router(opts.createHistory, opts.routes.map(createRouteNode))
 }
