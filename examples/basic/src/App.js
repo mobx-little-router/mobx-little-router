45084f28684e56dede18fae890afecda2de65750
Corban Brook <corbanbrook@gmail.com>
2017-08-15 17:10:27 -0400

    Adding styled components and Header to basic example.

--- examples/basic/src/App.js
+++ examples/basic/src/App.js
@@ -4,6 +4,9 @@ import { observer } from 'mobx-react'
 import { createHashHistory } from 'history'
 import { install, RouterStore } from 'mobx-little-router'
 import { RouterProvider, Link } from 'mobx-little-router-react'
+import styled, { injectGlobal } from 'styled-components'
+
+import Header from './components/Header'
 
 const Index = () => <div>Index</div>
 
@@ -18,7 +21,6 @@ const module = install({
   routes: [
     { path: '', data: { component: Index } },
     { path: 'about', data: { component: About } },
-    { path: 'asd', data: { component: About } },
     {
       path: 'posts/:id',
       data: { component: Post }
@@ -31,21 +33,45 @@ module.start()
 window.store = module.store
 
 class App extends Component {
+  collection = observable([])
+
+  componentDidMount = async () => {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const data = await res.json()
+
+    data.forEach(({ show }) => this.collection.push(show))
+  }
+
   render() {
     const { location } = module.store
     return (
       <RouterProvider module={module}>
         <div>
+          <Header />
           <p>pathname = {JSON.stringify(location.pathname)}</p>
-          <Link to="/">Index</Link>
-          <Link to="/about">About</Link>
-          <Link to="/asd">asd</Link>
-          <Link to="/posts/1">Post 1</Link>
-          Hello
+          <ul>
+            {this.collection.map(show =>
+              <li key={show.id}>
+                <Link to={`/posts/${show.id}`}>{show.name}</Link>
+              </li>
+            )}
+          </ul>
         </div>
       </RouterProvider>
     )
   }
 }
+
+injectGlobal`
+  body {
+    font-family: "Helvetica Neue", sans-serif;
+    padding: 0;
+    margin: 0;
+
+    * {
+      box-sizing: border-box;
+    }
+  }
+`
 
 export default observer(App)
---
+++ examples/basic/src/components/Header.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Link } from 'mobx-little-router-react'
+import styled from 'styled-components'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <Container>
+        <ul>
+          <li className="title">Basic Example</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </Container>
+    )
+  }
+}
+
+const Container = styled.div`
+  background-color: #333;
+  height: 54px;
+  line-height: 54px;
+
+  ul {
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    padding: 0;
+    margin: 0;
+
+    li {
+      &.title {
+        background-color: rgb(212, 212, 50);
+        padding: 0 18px;
+        color: white;
+      }
+
+      a {
+        display: block;
+        padding: 0 18px;
+        color: #ccc;
+        text-decoration: none;
+        font-size: 14px;
+
+        &.active, &:hover {
+          background-color: rgb(50, 212, 212);
+          color: white;
+        }
+      }
+    }
+  }
+`
