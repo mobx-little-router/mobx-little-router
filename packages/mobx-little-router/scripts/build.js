1e857e2fde45ccdb7b11b68f1d0c12f00a0c408d
Jack Hsu <jack.hsu@gmail.com>
2017-08-25 10:45:29 -0400

    Re-enables UMD build with a flag.

--- packages/mobx-little-router/scripts/build.js
+++ packages/mobx-little-router/scripts/build.js
@@ -11,8 +11,6 @@ const exec = (command, extraEnv) =>
 
 const BABEL_BIN = `${process.cwd()}/node_modules/.bin/babel`
 
-console.log('Building CommonJS modules ...')
-
 exec(`${BABEL_BIN} src -d lib --ignore "*.test.js"`, {
   BABEL_ENV: 'cjs'
 })
@@ -27,22 +25,24 @@ console.log('\nBuilding Flow modules ...')
 
 exec('./node_modules/.bin/flow-copy-source -v -i "**/*.test.js" src lib')
 
-// console.log('\nBuilding mobx-router-little.js ...')
-//
-// exec('rollup -c -f umd -o umd/mobx-router-little.js', {
-//   BABEL_ENV: 'umd',
-//   NODE_ENV: 'development'
-// })
-//
-// console.log('\nBuilding mobx-router-little.min.js ...')
-//
-// exec('rollup -c -f umd -o umd/mobx-router-little.min.js', {
-//   BABEL_ENV: 'umd',
-//   NODE_ENV: 'production'
-// })
-//
-// const size = gzipSize.sync(
-//   fs.readFileSync('umd/mobx-router-little.min.js')
-// )
-//
-// console.log('\ngzipped, the UMD build is %s', prettyBytes(size))
+if (process.argv.includes('umd')) {
+  console.log('\nBuilding mobx-router-little.js ...')
+
+  exec('rollup -c -f umd -o umd/mobx-router-little.js', {
+    BABEL_ENV: 'umd',
+    NODE_ENV: 'development'
+  })
+
+  console.log('\nBuilding mobx-router-little.min.js ...')
+
+  exec('rollup -c -f umd -o umd/mobx-router-little.min.js', {
+    BABEL_ENV: 'umd',
+    NODE_ENV: 'production'
+  })
+
+  const size = gzipSize.sync(
+    fs.readFileSync('umd/mobx-router-little.min.js')
+  )
+
+  console.log('\ngzipped, the UMD build is %s', prettyBytes(size))
+}
