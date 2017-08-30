24e343e1a3d493ea9778af72a0a2bc8f8789da40
Corban Brook <corbanbrook@gmail.com>
2017-08-30 12:53:55 -0400

    When comparing if routes are equal, also check route params.

--- packages/mobx-little-router/src/model/areRoutesEqual.js
+++ packages/mobx-little-router/src/model/areRoutesEqual.js
@@ -1,6 +1,7 @@
 // @flow
 import type { RouteNode } from './types'
+import shallowEqual from '../util/shallowEqual'
 
 export default function areRoutesEqual(a: RouteNode<*>, b: RouteNode<*>) {
-  return a.value.key === b.value.key
+  return a.value.key === b.value.key && shallowEqual(a.value.params, b.value.params)
 }
