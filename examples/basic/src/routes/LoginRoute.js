bafc8543755c0750889497c6e104f93ab40b27fd
Corban Brook <corbanbrook@gmail.com>
2017-08-30 11:26:58 -0400

    Adding styling to Login and fixing the redirectTo when there is no unauthorizedNavigation.

--- examples/basic/src/routes/LoginRoute.js
+++ examples/basic/src/routes/LoginRoute.js
@@ -11,7 +11,12 @@ class LoginRoute extends Component {
     
     runInAction(() => {
       SessionStore.isAuthenticated = true
-      router.push(SessionStore.unauthorizedNavigation.to)
+
+      const redirectTo = SessionStore.unauthorizedNavigation
+        ? SessionStore.unauthorizedNavigation.to
+        : '/'
+
+      router.push(redirectTo)
     })
   }
 
@@ -20,15 +25,59 @@ class LoginRoute extends Component {
 
     return (
       <Container className={className}>
-        <div><input type="text" placeholder="Email"/></div>
-        <div><input type="password" placeholder="Password"/></div>
-        <div><button onClick={this.login}>Login</button></div>
+        <h1>Login</h1>
+        <div><EmailInput/></div>
+        <div><PasswordInput/></div>
+        <div><LoginButton onClick={this.login}>Login</LoginButton></div>
       </Container>
     )
   }
 }
 
 const Container = styled.div`
+  margin: 0 auto;
+  width: 320px;
+`
+
+const Input = styled.input`
+  width: 100%;
+  height: 36px;
+  line-height: 34px;
+  border: 1px solid #eee;
+  margin-bottom: 18px;
+  padding: 0 9px;
+  border-radius: 2px;
+  transition: border-color 200ms ease-out;
+
+  &:focus {
+    border-color: #ccc;
+  }
+`
+
+const EmailInput = styled(Input).attrs({
+  type: 'text',
+  placeholder: 'Email'
+})``
+
+const PasswordInput = styled(Input).attrs({
+  type: 'password',
+  placeholder: 'Password'
+})``
+
+const LoginButton = styled.button`
+  height: 36px;
+  background-color: #333;
+  color: white;
+  border: 1px solid rgba(0,0,0,0.2);
+  border-radius: 2px;
+  min-width: 120px;
+  float: right;
+  cursor: pointer;
+  transition: background-color 200ms ease-out;
+
+  &:hover {
+    background-color: rgb(50, 212, 212);
+  }
 `
 
 export default withRouter(inject('SessionStore')(observer(LoginRoute)))
