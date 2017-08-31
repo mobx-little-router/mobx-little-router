8b5d8ebe9dce062d40ac725a54ed16803fe7dd9c
Corban Brook <corbanbrook@gmail.com>
2017-08-31 16:30:07 -0400

    No longer require to shallowCompare routeParams because segment is baked into route key.

--- packages/mobx-little-router/src/model/util/areRoutesEqual.js
+++ packages/mobx-little-router/src/model/util/areRoutesEqual.js
@@ -1,7 +1,6 @@
 // @flow
 import type { Route } from '../types'
-import shallowEqual from '../../util/shallowEqual'
 
 export default function areRoutesEqual(a?: Route<*, *>, b?: Route<*, *>) {
-  return !!(a && b) && a.key === b.key && shallowEqual(a.params, b.params)
+  return !!(a && b) && a.key === b.key
 }
