72dbd180821f28f595decf3b868e7c46861d935a
Jack Hsu <jack.hsu@gmail.com>
2017-09-08 14:12:20 -0400

    More tests for shallowEqual.

--- packages/mobx-little-router/src/util/shallowEqual.test.js
+++ packages/mobx-little-router/src/util/shallowEqual.test.js
@@ -6,4 +6,13 @@ describe('shallowEqual', () => {
     expect(shallowEqual({ a: 1 }, { a: 1 })).toBe(true)
     expect(shallowEqual({ a: 1 }, { a: 2 })).toBe(false)
   })
+
+  test('true when same instance', () => {
+    const a = {}
+    expect(shallowEqual(a, a)).toBe(true)
+  })
+
+  test('false when keys are not symmetrical', () => {
+    expect(shallowEqual({ a: 1 }, { a: 1, b: 2 })).toBe(false)
+  })
 })
