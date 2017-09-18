965a87c1e61eee47b5cf13d012fea54958a2d5be
Corban Brook <corbanbrook@gmail.com>
2017-09-18 18:19:18 -0400

    Don't add query property to navigation if it is empty.

--- packages/mobx-little-router/src/scheduling/util/withQueryMiddleware.js
+++ packages/mobx-little-router/src/scheduling/util/withQueryMiddleware.js
@@ -5,7 +5,7 @@ import transformNavigation from '../../middleware/transformNavigation'
 
 export default transformNavigation(navigation => {
   const { to } = navigation
-  if (to && typeof to.search === 'string') {
+  if (to && typeof to.search === 'string' && to.search.length > 1) {
     return new Navigation({
       ...navigation,
       to: { ...to, query: QueryString.parse(to.search.substr(1)) }
