60b54c8182d2b895c1378ed8301d4fb6b84844f5
Corban Brook <corbanbrook@gmail.com>
2017-09-18 18:17:28 -0400

    Fixing withQueryMiddleware inproper use of navigation.next preventing proper from route from being passed and increasing the sequence number.

--- packages/mobx-little-router/src/scheduling/util/withQueryMiddleware.js
+++ packages/mobx-little-router/src/scheduling/util/withQueryMiddleware.js
@@ -1,11 +1,12 @@
 // @flow
+import Navigation from '../../model/Navigation'
 import QueryString from 'qs'
 import transformNavigation from '../../middleware/transformNavigation'
 
 export default transformNavigation(navigation => {
   const { to } = navigation
   if (to && typeof to.search === 'string') {
-    return navigation.next({
+    return new Navigation({
       ...navigation,
       to: { ...to, query: QueryString.parse(to.search.substr(1)) }
     })
