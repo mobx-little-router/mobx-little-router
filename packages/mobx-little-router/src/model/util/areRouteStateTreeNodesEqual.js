43620a20d3ce0360a651c1f6281ce5a06613c68c
Jack Hsu <jack.hsu@gmail.com>
2017-09-21 10:22:20 -0400

    removes unused file

--- packages/mobx-little-router/src/model/util/areRouteStateTreeNodesEqual.js
+++
@@ -1,6 +0,0 @@
-// @flow
-import type { RouteStateTreeNode } from '../types'
-
-export default function areRouteStateTreeNodesEqual(a?: RouteStateTreeNode<*, *>, b?: RouteStateTreeNode<*, *>) {
-  return !!(a && b) && a.value.key === b.value.key
-}
