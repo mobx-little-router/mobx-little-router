6d2450932988fc4e7bd06ac05d52b488ba754323
Jack Hsu <jack.hsu@gmail.com>
2017-09-27 16:59:33 -0400

    removes validation middleware

--- packages/mobx-little-router/src/middleware/devTools/index.js
+++ packages/mobx-little-router/src/middleware/devTools/index.js
@@ -1,10 +1,6 @@
 // @flow
 const devTools = process.env.NODE_ENV !== 'production'
-  ? (
-    require('./validateConfigMiddleware').default.concat(
-      require('./timerMiddleware').default(() => Date.now())
-    )
-  )
+  ? require('./timerMiddleware').default(() => Date.now())
   : require('../Middleware').default.EMPTY
 
 export default devTools
