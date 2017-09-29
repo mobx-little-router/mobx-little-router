641aec2302161f096585bbac82e0526542e947a8
Jack Hsu <jack.hsu@gmail.com>
2017-09-29 14:00:02 -0400

    Updates React example

--- examples/react-client/src/routes/shows/index.js
+++ examples/react-client/src/routes/shows/index.js
@@ -9,18 +9,21 @@ export default [
     query: ['q'],
     willResolve: async route => {
       const { ShowsStore } = route.context.stores
+      let data
 
       // Faking some network order and latency issues.
       await delay(Math.random() * 500)
+
       if (!route.query.q) {
-        return () => ShowsStore.load([])
+        data = []
       } else {
         const res = await fetch(`https://api.tvmaze.com/search/shows?q=${route.query.q}`)
-        const data = await res.json()
-        return () => {
-          ShowsStore.load(data.map(({ show }) => show))
-        }
+        const json = await res.json()
+        data = json.map(({ show }) => show)
       }
+
+      // This returns a setter function that will be called by Router (assuming navigation is not cancelled).
+      return () => ShowsStore.load(data)
     },
     component: ShowsRoute,
     children: [
