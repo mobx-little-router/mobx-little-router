00038b7ded2e4b58e6dcf135e5349e3ffb5f3d49
Jack Hsu <jack.hsu@gmail.com>
2017-10-10 12:58:57 -0400

    Removes goBackOrElse since behaviour cannot be guaranteed.

--- packages/mobx-little-router/src/Router.js
+++ packages/mobx-little-router/src/Router.js
@@ -121,14 +121,6 @@ class Router {
     return this.navigated()
   }
 
-  goBackOrElse(href: Href) {
-    return (
-      this.scheduler.currentNavigation.shouldTransition
-        ? this.goBack()
-        : this.push(href)
-    ) 
-  }
-
   /* Private helpers */
 
   // Waits for next navigation event to be processed and resolves.
