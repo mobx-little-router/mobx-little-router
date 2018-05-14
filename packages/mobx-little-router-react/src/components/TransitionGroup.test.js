ff355b588dddd6a486de5956f9ba2324c5c657f7
Corban Riley <corbanbrook@gmail.com>
2018-05-14 11:43:24 -0400

    Fixing tests for enzyme 3.

--- packages/mobx-little-router-react/src/components/TransitionGroup.test.js
+++ packages/mobx-little-router-react/src/components/TransitionGroup.test.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { createRouter, delay } from '../testUtil'
 import { mount } from 'enzyme'
-import TransitionGroup from './TransitionGroup'
+import TransitionGroup, { TransitionItem } from './TransitionGroup'
 import { areRoutesEqual } from 'mobx-little-router'
 
 describe('TransitionGroup', () => {
@@ -19,6 +19,10 @@ describe('TransitionGroup', () => {
       (canTransition(to) || canTransition(from))
 
     wrapper.setProps({ to, from, isTransitioning })
+  }
+
+  const hasClass = (el, className) => {
+    return el.html().indexOf(className) >= 0
   }
 
   beforeEach(() => {
@@ -48,25 +52,43 @@ describe('TransitionGroup', () => {
     router.stop()
   })
 
-  test('TransitionGroup handles transitioning in and out of routes', async () => {
+  test('TransitionGroup initial state is empty', async () => {
+    const wrapper = mount(<TransitionGroup isTransitioning={false} />)
+    expect(wrapper.find(TransitionItem).length).toBe(0)
+  })
+
+  test('TransitionGroup has TransitionItem after route change', async () => {
     const wrapper = mount(<TransitionGroup isTransitioning={false} />)
     
-    expect(wrapper.children().length).toBe(0)
+    router.push('/about')
+    await delay(0)
+    updateRoutes(wrapper)
+
+    expect(wrapper.find(TransitionItem).length).toBe(1)
+  })
+
+  // XXX note that classList mutations are no longer picked up by enzyme
+  // We need to mutate the classlist in this fashion to trigger the animation in the most
+  // efficient way possible. The hasClass helper uses enzymes .html() function to view the raw html
+  // we use this to check the animation lifecycle classes are correctly applied.
+  test('TransitionGroup handles transitioning in and out of routes', async () => {
+    const wrapper = mount(<TransitionGroup isTransitioning={false} />)
+    const { routes, prevRoutes } = router._store
 
     router.push('/about')
     await delay(0)
     updateRoutes(wrapper)
 
     // Initially we should be transitioning and have the enter class
-    expect(wrapper.children().length).toBe(1)
-    expect(wrapper.childAt(0).childAt(0).hasClass('transitioning')).toBe(true)  
-    expect(wrapper.childAt(0).childAt(0).hasClass('enter')).toBe(true)
-    
-    await delay(0)
+    expect(wrapper.find(TransitionItem).hasClass('transitioning')).toBe(true)
+    expect(wrapper.find(TransitionItem).hasClass('enter')).toBe(true)
+
+    await delay(1000)
 
     // Then the animation is initialized with the active class
-    expect(wrapper.childAt(0).childAt(0).hasClass('enter-active')).toBe(true)
-    expect(router._store.routes[1].data.transitionState).toBe('entering')
+    expect(routes[1].data.transitionState).toBe('entering')
+    //XXX expect(wrapper.find(TransitionItem).hasClass('enter-active')).toBe(true)
+    expect(hasClass(wrapper.find(TransitionItem).at(0), 'enter-active')).toBe(true)
 
     // Wait for transition to complete
     await delay(150)
@@ -75,34 +97,36 @@ describe('TransitionGroup', () => {
     // Our transition has settled and transitioning class removed 
     expect(wrapper.childAt(0).childAt(0).hasClass('transitioning')).toBe(false)
     expect(wrapper.childAt(0).childAt(0).hasClass('enter-active')).toBe(false)
-    expect(router._store.routes[1].data.transitionState).toBe('entered')
+    expect(routes[1].data.transitionState).toBe('entered')
 
     router.push('/contact')
     await delay(0)
     updateRoutes(wrapper)
 
     // Now we will have two transitioning elements
-    expect(wrapper.children().length).toBe(2)
-    expect(wrapper.childAt(0).childAt(0).hasClass('transitioning')).toBe(true)
-    expect(wrapper.childAt(1).childAt(0).hasClass('transitioning')).toBe(true)
-    expect(wrapper.childAt(0).childAt(0).hasClass('exit')).toBe(true)
-    expect(wrapper.childAt(1).childAt(0).hasClass('enter')).toBe(true)
+    expect(wrapper.find(TransitionItem).length).toBe(2)
+    expect(wrapper.find(TransitionItem).at(0).hasClass('transitioning')).toBe(true)
+    expect(wrapper.find(TransitionItem).at(1).hasClass('transitioning')).toBe(true)
+    expect(wrapper.find(TransitionItem).at(0).hasClass('exit')).toBe(true)
+    expect(wrapper.find(TransitionItem).at(1).hasClass('enter')).toBe(true)
 
     await delay(0)
 
     // Then the animation is initialized with the active class
-    expect(wrapper.childAt(0).childAt(0).hasClass('exit-active')).toBe(true)
-    expect(wrapper.childAt(1).childAt(0).hasClass('enter-active')).toBe(true)
-    expect(router._store.routes[1].data.transitionState).toBe('entering')
-    expect(router._store.prevRoutes[1].data.transitionState).toBe('exiting')
+    expect(routes[1].data.transitionState).toBe('entering')
+    expect(prevRoutes[1].data.transitionState).toBe('exiting')
+    //XXX expect(wrapper.find(TransitionItem).at(0).hasClass('exit-active')).toBe(true)
+    //XXX expect(wrapper.find(TransitionItem).at(1).hasClass('enter-active')).toBe(true)
+    expect(hasClass(wrapper.find(TransitionItem).at(0), 'exit-active')).toBe(true)
+    expect(hasClass(wrapper.find(TransitionItem).at(1), 'enter-active')).toBe(true)
 
     // Wait for the transition to complete
     await delay(150)
     updateRoutes(wrapper)
 
     // Our transition has settled and transitioning class removed 
-    expect(wrapper.children().length).toBe(1)
-    expect(wrapper.childAt(0).childAt(0).hasClass('transitioning')).toBe(false)
+    expect(wrapper.find(TransitionItem).length).toBe(1)
+    expect(wrapper.find(TransitionItem).hasClass('transitioning')).toBe(false)
   })
 })
 
