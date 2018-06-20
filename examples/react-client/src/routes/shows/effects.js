c126d1565a0e039d8c7dc5968f810e82d20aacc9
Corban Riley <corbanbrook@gmail.com>
2018-06-20 14:34:26 -0400

    Refactoring effects to use generators

--- examples/react-client/src/routes/shows/effects.js
+++ examples/react-client/src/routes/shows/effects.js
@@ -2,39 +2,39 @@
  * This module contains effects that exist within the `shows` route.
  * Effects run as a reaction to route params and query changes.
  */
-import { autorun, action } from 'mobx'
+import { autorun, action, flow } from 'mobx'
 
 
 
 export const fetchShows = ({ route, stores }) => {
-  const updatePending = action((value) => {
-    route.state.isPending = value
-  })
-
   return autorun(() => {
-    if (route.query.q) {
-      updatePending(true)
-      fetch(`https://api.tvmaze.com/search/shows?q=${route.query.q}`)
-        .then(res => res.json())
-        .then(json => json.map(({ show }) => show))
-        .then(data => stores.ShowsStore.load(data))
-        .then(() => updatePending(false))
+    const { state, query } = route
+
+    if (query.q) {
+      flow(function* () {
+        state.isPending = true
+        const res = yield fetch(`https://api.tvmaze.com/search/shows?q=${query.q}`)
+        const json = yield res.json()
+        const data = yield json.map(({ show }) => show)
+        stores.ShowsStore.load(data)
+        state.isPending = false
+      })()
     }
   }, { delay: 200 })
 }
 
 export function fetchShow({ route, stores }) {
-  const updatePending = action((value) => {
-    route.state.isPending = value
-  })
-
   return autorun(() => {
+    const { state, params } = route
+
     if (route.params.id) {
-      updatePending(true)
-      fetch(`https://api.tvmaze.com/shows/${route.params.id}?embed=cast`)
-        .then(res => res.json())
-        .then(data => stores.ShowsStore.loadDetails(route.params.id, data))
-        .then(() => updatePending(false))
-    }
+      flow(function* () {
+        state.isPending = true
+        const res = yield fetch(`https://api.tvmaze.com/shows/${params.id}?embed=cast`)
+        const json = yield res.json()
+        stores.ShowsStore.loadDetails(params.id, json)
+        state.isPending = false
+      })()
+    } 
   })
 }
