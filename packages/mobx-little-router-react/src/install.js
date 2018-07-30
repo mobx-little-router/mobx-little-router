4d16b1300d11d845a49b87b38ab1e4a2c24949f5
Jack Hsu <jack.hsu@gmail.com>
2018-07-30 13:05:50 -0400

    Fixes imports

--- packages/mobx-little-router-react/src/install.js
+++ packages/mobx-little-router-react/src/install.js
@@ -1,7 +1,7 @@
 // @flow
-import type { InstallOptions } from '../../mobx-little-router/es'
-import { Router } from '../../mobx-little-router/es'
-import * as router from '../../mobx-little-router/es'
+import type { InstallOptions } from 'mobx-little-router'
+import { Router } from 'mobx-little-router'
+import * as router from 'mobx-little-router'
 import transformReactConfig from './middleware/transformReactConfig'
 
 export default function install(opts: InstallOptions): Router {
